b57e9f487857355e0e6fc0df91511bb06b93f45b
Spyro1 <redstone.mc01@gmail.com>
2024-02-26 19:29:34 +0100

    Print main screen with bitmaps v1

--- GUI_teszt/GUI_teszt.ino
+++ GUI_teszt/GUI_teszt.ino
@@ -1,3 +1,7 @@
+/*#include <SPI.h>
+#include <Adafruit_ILI9341.h>
+#include <Adafruit_ImageReader.h>*/
+#include <SD.h>
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_TFTLCD.h> // Hardware-specific library
 #include <TouchScreen.h>
@@ -24,9 +28,9 @@ TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 #define LCD_CD A2
 #define LCD_WR A1
 #define LCD_RD A0
+#define SD_CS 10     // Set the chip select line to whatever you use (10 doesnt conflict with the library)
 // optional
 #define LCD_RESET A4
-
 // 16 bites szinek
 #define BLACK 0x0000
 #define BLUE 0x001F
@@ -36,7 +40,7 @@ TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 #define MAGENTA 0xF81F
 #define YELLOW 0xFFE0
 #define WHITE 0xFFFF
-#define DARKCYAN 0x0472
+#define DARKCYAN 0x032D //0472
 
 // Menü makrók
 #define MARGIN_V 4  // Vertical margin
@@ -52,10 +56,8 @@ TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 
 Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
 
-using namespace std;
-
 // == Objects and global variables ==
-class MyButton 
+/*class MyButton 
 {
   uint16_t backColor;
   uint16_t borderColor;
@@ -79,30 +81,49 @@ class MyButton
   {
   }
 
-};
+};*/
 
 Adafruit_GFX_Button button1;
 
+/*SdFat                SD;         // SD card filesystem
+Adafruit_ImageReader reader(SD); // Image-reader object, pass in SD filesys
+*/
 // == Functions ==
 
-void
-DrawMainScreen()
+void DrawMainScreen()
 {
   tft.fillScreen(DARKCYAN);
 
-  button1.initButton(&tft, 0,0,50,50, WHITE, YELLOW, WHITE, "Gomb", 2); // GOMB ?????
-  button1.drawButton();
 
 
-  tft.drawRoundRect(MARGIN_H, MARGIN_V, ICONSIZE, ICONSIZE, 5, CYAN);                    // Bal felső ikon
-  tft.drawRoundRect(MARGIN_H * 3 + ICONSIZE, MARGIN_V, ICONSIZE, ICONSIZE, 5, CYAN);     // Bal fent közép ikon
-  tft.drawRoundRect(MARGIN_H * 5 + ICONSIZE * 2, MARGIN_V, ICONSIZE, ICONSIZE, 5, CYAN); // Jobb fent közép ikon
-  tft.drawRoundRect(MARGIN_H * 7 + ICONSIZE * 3, MARGIN_V, ICONSIZE, ICONSIZE, 5, CYAN); // Jobb fent ikon
+  //button1.initButton(&tft, 0,0,50,50, WHITE, YELLOW, WHITE, "Gomb", 2); // GOMB ?????
+  //button1.drawButton();
+
+  /////
+  
+  /*uint8_t  sdbuffer[3*20];
+  if ((bmpFile = SD.open(filename)) == NULL) {
+    Serial.println(F("File not found"));
+    return;
+  }
+  bmpFile.read(sdbuffer, sizeof(sdbuffer));
+  tft.drawBitmap(MARGIN_H, MARGIN_V, sdbuffer, ICONSIZE, ICONSIZE, BLACK);*/
+  //=====reader.drawBMP("/settings.bmp", tft, 0, 0);
+  //////
+  //tft.drawBitmap();
+  bmpDraw("on-sprinkler.bmp", MARGIN_H, MARGIN_V);
+  bmpDraw("chain.bmp", MARGIN_H * 3 + ICONSIZE, MARGIN_V);
+  bmpDraw("on-off.bmp", MARGIN_H * 5 + ICONSIZE * 2, MARGIN_V);
+  bmpDraw("humidity.bmp", MARGIN_H * 7 + ICONSIZE * 3, MARGIN_V);
+  //tft.drawRoundRect(MARGIN_H, MARGIN_V, ICONSIZE, ICONSIZE, 5, CYAN);                    // Bal felső ikon
+  //tft.drawRoundRect(MARGIN_H * 3 + ICONSIZE, MARGIN_V, ICONSIZE, ICONSIZE, 5, CYAN);     // Bal fent közép ikon
+  //tft.drawRoundRect(MARGIN_H * 5 + ICONSIZE * 2, MARGIN_V, ICONSIZE, ICONSIZE, 5, CYAN); // Jobb fent közép ikon
+  //tft.drawRoundRect(MARGIN_H * 7 + ICONSIZE * 3, MARGIN_V, ICONSIZE, ICONSIZE, 5, CYAN); // Jobb fent ikon
 
   tft.setCursor(17, MARGIN_V * 2 + ICONSIZE);
   tft.setTextColor(WHITE);
   tft.setTextSize(1);
-  tft.print("Locsolas      Sorban      Teszteles     Idozito");
+  tft.print("Locsolas      Sorban      Teszteles     Nedvesseg");
 
   tft.setCursor(26, MARGIN_V * 6 + ICONSIZE + FONT_1_V);
   tft.setTextColor(CYAN);
@@ -117,9 +138,13 @@ DrawMainScreen()
   tft.setTextSize(2);
   tft.print("12:02:16"); // Valós óra
 
-  tft.drawRoundRect(48, 154, ICONSIZE, ICONSIZE, 5, CYAN);  // Bal alsó ikon
-  tft.drawRoundRect(128, 154, ICONSIZE, ICONSIZE, 5, CYAN); // Közép alsó ikon
-  tft.drawRoundRect(208, 154, ICONSIZE, ICONSIZE, 5, CYAN); // Jobb alsó ikon
+  
+  bmpDraw("settings.bmp", 48, 154);
+  bmpDraw("on-button.bmp", 128, 154);
+  bmpDraw("clock.bmp", 208, 154);
+  //tft.drawRoundRect(48, 154, ICONSIZE, ICONSIZE, 5, CYAN);  // Bal alsó ikon
+  //tft.drawRoundRect(128, 154, ICONSIZE, ICONSIZE, 5, CYAN); // Közép alsó ikon
+  //tft.drawRoundRect(208, 154, ICONSIZE, ICONSIZE, 5, CYAN); // Jobb alsó ikon
 
   tft.setCursor(56, 222);
   tft.setTextColor(WHITE);
@@ -167,9 +192,9 @@ void setup(void)
   Serial.println(F("Paint!"));
 
   tft.reset();
-
-  // Get LCD driver name
   uint16_t identifier = tft.readID();
+  /*
+  // Get LCD driver name
   if (identifier == 0x9325)
     Serial.println(F("Found ILI9325 LCD driver"));
   else if (identifier == 0x9328)
@@ -192,14 +217,25 @@ void setup(void)
     Serial.println(F("matches the tutorial."));
     return;
   }
+  */
 
   // Set tft panel
-
   tft.begin(identifier);
   pinMode(13, OUTPUT);
   tft.setRotation(3);
-
   tft.fillScreen(DARKCYAN);
+  //tft.fillScreen(BLACK);
+
+
+  // Setup SD card
+  Serial.print(F("Initializing SD card..."));
+  if (!SD.begin(SD_CS)) {
+    Serial.println(F("Failed!"));
+    //return;
+  }
+  Serial.println(F("OK!"));
+
+  //bmpDraw("clock.bmp", 0, 0);
 
   DrawMainScreen();
   // DrawPeriodsMenu();
@@ -245,3 +281,148 @@ void loop()
     Serial.println(")");
   }
 }
+
+
+// Helper functions
+#define BUFFPIXEL 20
+void bmpDraw(char *filename, int x, int y) {
+
+  File     bmpFile;
+  int      bmpWidth, bmpHeight;   // W+H in pixels
+  uint8_t  bmpDepth;              // Bit depth (currently must be 24)
+  uint32_t bmpImageoffset;        // Start of image data in file
+  uint32_t rowSize;               // Not always = bmpWidth; may have padding
+  uint8_t  sdbuffer[3*BUFFPIXEL]; // pixel in buffer (R+G+B per pixel)
+  uint16_t lcdbuffer[BUFFPIXEL];  // pixel out buffer (16-bit per pixel)
+  uint8_t  buffidx = sizeof(sdbuffer); // Current position in sdbuffer
+  boolean  goodBmp = false;       // Set to true on valid header parse
+  boolean  flip    = true;        // BMP is stored bottom-to-top
+  int      w, h, row, col;
+  uint8_t  r, g, b;
+  uint32_t pos = 0, startTime = millis();
+  uint8_t  lcdidx = 0;
+  boolean  first = true;
+
+  if((x >= tft.width()) || (y >= tft.height())) return;
+
+  Serial.println();
+  Serial.print(F("Loading image '"));
+  Serial.print(filename);
+  Serial.println('\'');
+  // Open requested file on SD card
+  if ((bmpFile = SD.open(filename)) == NULL) {
+    Serial.println(F("File not found"));
+    return;
+  }
+
+  // Parse BMP header
+  if(read16(bmpFile) == 0x4D42) { // BMP signature
+    Serial.println(F("File size: ")); Serial.println(read32(bmpFile));
+    (void)read32(bmpFile); // Read & ignore creator bytes
+    bmpImageoffset = read32(bmpFile); // Start of image data
+    Serial.print(F("Image Offset: ")); Serial.println(bmpImageoffset, DEC);
+    // Read DIB header
+    Serial.print(F("Header size: ")); Serial.println(read32(bmpFile));
+    bmpWidth  = read32(bmpFile);
+    bmpHeight = read32(bmpFile);
+    if(read16(bmpFile) == 1) { // # planes -- must be '1'
+      bmpDepth = read16(bmpFile); // bits per pixel
+      Serial.print(F("Bit Depth: ")); Serial.println(bmpDepth);
+      if((bmpDepth == 24) && (read32(bmpFile) == 0)) { // 0 = uncompressed
+
+        goodBmp = true; // Supported BMP format -- proceed!
+        Serial.print(F("Image size: "));
+        Serial.print(bmpWidth);
+        Serial.print('x');
+        Serial.println(bmpHeight);
+
+        // BMP rows are padded (if needed) to 4-byte boundary
+        rowSize = (bmpWidth * 3 + 3) & ~3;
+
+        // If bmpHeight is negative, image is in top-down order.
+        // This is not canon but has been observed in the wild.
+        if(bmpHeight < 0) {
+          bmpHeight = -bmpHeight;
+          flip      = false;
+        }
+
+        // Crop area to be loaded
+        w = bmpWidth;
+        h = bmpHeight;
+        if((x+w-1) >= tft.width())  w = tft.width()  - x;
+        if((y+h-1) >= tft.height()) h = tft.height() - y;
+
+        // Set TFT address window to clipped image bounds
+        tft.setAddrWindow(x, y, x+w-1, y+h-1);
+
+        for (row=0; row<h; row++) { // For each scanline...
+          // Seek to start of scan line.  It might seem labor-
+          // intensive to be doing this on every line, but this
+          // method covers a lot of gritty details like cropping
+          // and scanline padding.  Also, the seek only takes
+          // place if the file position actually needs to change
+          // (avoids a lot of cluster math in SD library).
+          if(flip) // Bitmap is stored bottom-to-top order (normal BMP)
+            pos = bmpImageoffset + (bmpHeight - 1 - row) * rowSize;
+          else     // Bitmap is stored top-to-bottom
+            pos = bmpImageoffset + row * rowSize;
+          if(bmpFile.position() != pos) { // Need seek?
+            bmpFile.seek(pos);
+            buffidx = sizeof(sdbuffer); // Force buffer reload
+          }
+
+          for (col=0; col<w; col++) { // For each column...
+            // Time to read more pixel data?
+            if (buffidx >= sizeof(sdbuffer)) { // Indeed
+              // Push LCD buffer to the display first
+              if(lcdidx > 0) {
+                tft.pushColors(lcdbuffer, lcdidx, first);
+                lcdidx = 0;
+                first  = false;
+              }
+              bmpFile.read(sdbuffer, sizeof(sdbuffer));
+              buffidx = 0; // Set index to beginning
+            }
+
+            // Convert pixel from BMP to TFT format
+            b = sdbuffer[buffidx++];
+            g = sdbuffer[buffidx++];
+            r = sdbuffer[buffidx++];
+            lcdbuffer[lcdidx++] = tft.color565(r,g,b);
+          } // end pixel
+        } // end scanline
+        // Write any remaining data to LCD
+        if(lcdidx > 0) {
+          tft.pushColors(lcdbuffer, lcdidx, first);
+        } 
+        Serial.print(F("Loaded in "));
+        Serial.print(millis() - startTime);
+        Serial.println(" ms");
+      } // end goodBmp
+    }
+  }
+
+  bmpFile.close();
+  if(!goodBmp) Serial.println(F("BMP format not recognized."));
+}
+
+// These read 16- and 32-bit types from the SD card file.
+// BMP data is stored little-endian, Arduino is little-endian too.
+// May need to reverse subscript order if porting elsewhere.
+
+uint16_t read16(File f) {
+  uint16_t result;
+  ((uint8_t *)&result)[0] = f.read(); // LSB
+  ((uint8_t *)&result)[1] = f.read(); // MSB
+  return result;
+}
+
+uint32_t read32(File f) {
+  uint32_t result;
+  ((uint8_t *)&result)[0] = f.read(); // LSB
+  ((uint8_t *)&result)[1] = f.read();
+  ((uint8_t *)&result)[2] = f.read();
+  ((uint8_t *)&result)[3] = f.read(); // MSB
+  return result;
+}
+
