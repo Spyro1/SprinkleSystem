1c56ed824f75612a69e49a635077702a69848bf9
Spyro1 <redstone.mc01@gmail.com>
2024-06-28 14:11:25 +0200

    Function documentation: Time, button, Relay, Profile

--- MinimalistSprinklerGUI/b_button.ino
+++ MinimalistSprinklerGUI/b_button.ino
@@ -1,49 +1,64 @@
 // ******** button.ino ********
 
+/**
+ * @brief Represents a touch button on the user interface. 
+ * The TouchButton struct defines the position and size of a button and provides methods to determine if the button is pressed.
+ */
 struct TouchButton {
-    Point pos;       // Position of top left corner
-    Point size;       // Size of the button
+  Point pos;   /**< The position of the top left corner of the button. */
+  Point size;  /**< The size of the button. */
 
-    // -- Constructors --
-    /** Default constructor of TocuhButton. Used for arrays. */
-    TouchButton() : pos(0, 0), size(0, 0) {}
-    /**
-     * Constructor with basic parameters
-     * @param x top left corner of button
-     * @param y top left corner of button
-     * @param width width of the button
-     * @param height height of the button
-     */
-    TouchButton(const int x, const int y, const int width, const int height) : pos(x, y), size(width, height) {}
-    /**
-     * Constructor with advanced parameters
-     * @param position top left corner of button
-     * @param size width and height of the button
-     */
-    TouchButton(const Point& position, const Point& size) : pos(position), size(size) {}
-    /**
-     * Tests if the given coordinate is inside the button
-     * @param x x coordinate
-     * @param y y coordinate
-     * @return true if the coordinate is inside the button, false if outside
-     */
-    bool isPressed(const int x, const int y) const { return (x >= pos.x && x <= pos.x + size.x) && (y >= pos.y && y <= pos.y + size.y); }
-    /**
-     * Tests if the given coordinate is inside the button
-     * @param p coordinate point
-     * @return true if the coordinate is inside the button, false if outside
-     */
-    bool isPressed(const Point& p) const { return isPressed(p.x, p.y); }
-
+  /** 
+   * @brief Default constructor for TouchButton. 
+   * Initializes the button position and size to (0, 0). 
+   * This constructor is mainly used for creating arrays of buttons.
+   */
+  TouchButton() : pos(0, 0), size(0, 0) {}
+  /**
+   * @brief Constructor with basic parameters. 
+   * Initializes the button with the given position and size. 
+   * @param x The x-coordinate of the top left corner of the button.
+   * @param y The y-coordinate of the top left corner of the button.
+   * @param width The width of the button.
+   * @param height The height of the button.
+   */
+  TouchButton(const int x, const int y, const int width, const int height) : pos(x, y), size(width, height) {}
+  /**
+   * @brief Constructor with advanced parameters.
+   * Initializes the button with the given position and size. 
+   * @param position The top left corner of the button.
+   * @param size The width and height of the button.
+   */
+  TouchButton(const Point& position, const Point& size) : pos(position), size(size) {}
+  /**
+   * @brief Tests if the given coordinate is inside the button.
+   * @param x The x-coordinate to test.
+   * @param y The y-coordinate to test.
+   * @return True if the coordinate is inside the button, false otherwise.
+   */
+  bool isPressed(const int x, const int y) const { return (x >= pos.x && x <= pos.x + size.x) && (y >= pos.y && y <= pos.y + size.y); }
+  /**
+   * @brief Tests if the given coordinate is inside the button. 
+   * @param p The coordinate point to test.
+   * @return True if the coordinate is inside the button, false otherwise.
+   */
+  bool isPressed(const Point& p) const { return isPressed(p.x, p.y); }
 };
 
+/**
+ * @brief Array of main screen buttons.
+ */
 const TouchButton mainBtns[mainScreenButtonCount] = { TouchButton(MAIN_1, ICONSIZE), TouchButton(MAIN_2, ICONSIZE), TouchButton(MAIN_3, ICONSIZE), TouchButton(MAIN_4, ICONSIZE), TouchButton(MAIN_5, ICONSIZE), TouchButton(MAIN_6, ICONSIZE) };
-
+/**
+ * @brief Array of submenu buttons.
+ */
 const TouchButton subMenuBtns[subMenuButtonCount] = {
   TouchButton(SLOT_1_1, ICONSIZE), TouchButton(SLOT_1_2, ICONSIZE), TouchButton(SLOT_1_3, ICONSIZE), TouchButton(SLOT_1_4, ICONSIZE),
   TouchButton(SLOT_2_1, ICONSIZE), TouchButton(SLOT_2_2, ICONSIZE), TouchButton(SLOT_2_3, ICONSIZE), TouchButton(SLOT_2_4, ICONSIZE),
   TouchButton(SLOT_3_1, ICONSIZE), TouchButton(SLOT_3_2, ICONSIZE), TouchButton(SLOT_3_3, ICONSIZE), TouchButton(SLOT_3_4, ICONSIZE)
 };
-
+/**
+ * @brief Home button for the submenu.
+ */
 const TouchButton homeBtn(SUBMENU_HOME_POS, {x32, x32});
 
--- MinimalistSprinklerGUI/b_relay_profile.ino
+++ MinimalistSprinklerGUI/b_relay_profile.ino
@@ -1,55 +1,67 @@
 // ******** relay_profile.ino ********
 
-// ==== Relay ====
+/**
+ * @brief Represents a relay for controlling sprinklers. 
+ * The Relay struct defines a relay with properties such as pin number, state, start time, and duration.
+ */
 struct Relay
 {
-  static uint idCounter; // Max 256 relays available
-  uint id;               // Relay identifier
-  uint pin;              // Connected pin on the arduino
-  bool state;            // Iondicates wheter the relay is activated or not
-  // -- Timed Sprinkling --
-  Time start;       // Start time of sprinkling
-  Range60 duration; // Duration in minutes of sprinkling
+  static uint idCounter; /**< Counter for generating unique relay IDs. Maximum of 256 relays available. */
+  uint id;               /**< Unique identifier for the relay. */
+  uint pin;              /**< Pin number on the Arduino to which the relay is connected. */
+  bool state;            /**< Indicates whether the relay is activated or not. */
+  Time start;            /**< Start time of the sprinkling. */
+  Range60 duration;      /**< Duration of the sprinkling in minutes. */
 
   /**
-   * Constructor for instances
-   * @param pin Pin of the relay on board
-   * @param start Start time of automated sprinkling
-   * @param duration Duration of the sprinkling in minutes
-   * */
+   * @brief Constructor to initialize a Relay instance. 
+   * @param pin The pin number to which the relay is connected. Defaults to 0.
+   * @param start The start time of automated sprinkling. Defaults to 0.
+   * @param duration The duration of the sprinkling in minutes. Defaults to 0.
+   */
   Relay(uint8_t pin = 0, struct Time start = 0, uint duration = 0) : id(idCounter++), pin(pin), start(start), duration(duration) {}
-  // - Functions -
-  Time end() { return start + duration(); } // Convert duration from minutes to seconds
-  void SetRelayState(bool value)
-  {
+  /**
+   * @brief Calculates the end time of sprinkling. 
+   * @return The end time of the sprinkling.
+   */
+  Time end() { return start + duration(); }
+  /**
+   * @brief Sets the state of the relay. 
+   * @param value The desired state of the relay. True to activate, false to deactivate.
+   */
+  void SetRelayState(bool value) {
     state = value;
-    if (!state)
-      digitalWrite(pin, HIGH);
-    else
-      digitalWrite(pin, LOW);
+    if (!state) digitalWrite(pin, HIGH);
+    else digitalWrite(pin, LOW);
   }
-  void reset()
-  {
+  /**
+   * @brief Resets the relay's start time and duration.
+   */
+  void reset() {
     start = 0;
     duration = 0;
-    // state = false;
   }
 };
 
-uint8_t Relay::idCounter = 0; // Set default value to id counter
+uint8_t Relay::idCounter = 0; /**< Initialize the relay ID counter to 0. */
 
-// ==== Profile ====
+/**
+ * @brief Represents a profile containing multiple relays.
+ * The Profile struct defines a profile with an array of relays and an active state.
+ */
 struct Profile
 {
-  bool isActive;                 // !EEPROM!
-  Relay relays[MAX_RELAY_COUNT]; // !EEPROM!
+  bool isActive;                 /**< Indicates whether the profile is active. */
+  Relay relays[MAX_RELAY_COUNT]; /**< Array of relays in the profile. */
 
-  Profile() : isActive(false)
-  {
-    for (uint i = 0; i < MAX_RELAY_COUNT; i++)
-    {
+  /**
+   * @brief Constructor to initialize a Profile instance.
+   * The constructor sets the relays' pin numbers and their default states.
+   */
+  Profile() : isActive(false) {
+    for (uint i = 0; i < MAX_RELAY_COUNT; i++) {
       relays[i].pin = RELAY_PINS[i];            // Set pins to relays
-      relays[i].SetRelayState(relays[i].state); // set default state
+      relays[i].SetRelayState(relays[i].state); // Set default state
     }
   }
 };
